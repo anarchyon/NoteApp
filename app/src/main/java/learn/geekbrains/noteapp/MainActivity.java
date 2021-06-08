95dc9660bb3725cc3ceb7056dc23396b2538ccad
toygildinpv <protagonyst@mail.ru>
2021-06-09 00:38:48 +0400

    Исправлено

--- app/src/main/java/learn/geekbrains/noteapp/MainActivity.java
+++ app/src/main/java/learn/geekbrains/noteapp/MainActivity.java
@@ -2,34 +2,44 @@ package learn.geekbrains.noteapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.os.PersistableBundle;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements ListNotesFragment.Controller {
-    private Note note;
+    public static final String KEY_NOTE = "key_note";
+    private Note note = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        if (savedInstanceState != null) {
+            note = savedInstanceState.getParcelable(KEY_NOTE);
+        }
 
-    @Override
-    protected void onStart() {
-
+        clearBackStack();
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.fragment_container, new ListNotesFragment())
                 .commit();
 
         if (note != null) showNote();
+    }
 
-        super.onStart();
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (note != null) {
+            outState.putParcelable(KEY_NOTE, note);
+        }
     }
 
     @Override
@@ -38,9 +48,19 @@ public class MainActivity extends AppCompatActivity implements ListNotesFragment
         showNote();
     }
 
+    private void clearBackStack() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.getBackStackEntryCount() > 0) {
+            fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        }
+    }
+
     private void showNote() {
-        boolean isLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        boolean isLandscape = getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE;
         int fragmentIdForNote = isLandscape ? R.id.fragment_note_details : R.id.fragment_container;
+
+        clearBackStack();
 
         getSupportFragmentManager()
                 .beginTransaction()
