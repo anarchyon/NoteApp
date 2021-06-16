7447080ddd85eea9ee4585ad38a6e163ac564144
toygildinpv <protagonyst@mail.ru>
2021-06-16 17:25:23 +0400

    Добавлено создание новой заметки, добавлено редактирование заметки, боковое меню перестало реагировать на нажатия, буду доделывать в следующей домашке

--- app/src/main/java/learn/geekbrains/noteapp/NoteFragment.java
+++ app/src/main/java/learn/geekbrains/noteapp/NoteFragment.java
@@ -2,12 +2,19 @@ package learn.geekbrains.noteapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +25,8 @@ public class NoteFragment extends Fragment {
 
     private static final String ARG_NOTE = "note";
     private Note note;
+    private AppCompatEditText subjectNote, textNote;
+    private AppCompatTextView creationDate;
 
     public NoteFragment() {
     }
@@ -41,23 +50,77 @@ public class NoteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_note, container, false);
+        View view = inflater.inflate(R.layout.fragment_note, container, false);
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.bottom_note_menu, menu);
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        TextView header = view.findViewById(R.id.header);
-        TextView text = view.findViewById(R.id.text);
-        TextView creationDate = view.findViewById(R.id.creation_date);
+        subjectNote = view.findViewById(R.id.subject_note);
+        textNote = view.findViewById(R.id.text);
+        creationDate = view.findViewById(R.id.creation_date);
 
-        header.setText(note.getName());
-        text.setText(note.getText());
-        creationDate.setText(note.getCreationDate());
-
-//        super.onViewCreated(view, savedInstanceState);
+        fillNoteFields();
+        setMenuListener();
     }
 
-    public interface Controller {
+    private void setMenuListener() {
+        BottomAppBar bottomAppBar = requireActivity().findViewById(R.id.bottom_menu);
+        bottomAppBar.setOnMenuItemClickListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.menu_bottom_save_note) {
+                subjectNote.setFocusable(false);
+                textNote.setFocusable(false);
+                getContract().saveNote(gatherNote());
+            } else if (itemId == R.id.menu_bottom_edit_note) {
+                subjectNote.setFocusableInTouchMode(true);
+                textNote.setFocusableInTouchMode(true);
+            }
+            return true;
+        });
+    }
+
+    private Note gatherNote() {
+        String subject = getStringFromField(subjectNote.getText());
+        String text = getStringFromField(textNote.getText());
+        return new Note(
+                note == null ? Note.generateNewId() : note.getId(),
+                subject,
+                text,
+                note == null ? Note.getCurrentDate() : note.getCreationDate()
+        );
+    }
+
+    private String getStringFromField(Editable fieldContent) {
+        if (fieldContent == null || fieldContent.toString().trim().isEmpty()) {
+            return getResources().getString(R.string.note);
+        }
+        return fieldContent.toString();
+    }
+
+    private void fillNoteFields() {
+        if (note == null) {
+            subjectNote.setFocusableInTouchMode(true);
+            subjectNote.requestFocus();
+            textNote.setFocusableInTouchMode(true);
+            return;
+        };
+        subjectNote.setText(note.getName());
+        textNote.setText(note.getText());
+        creationDate.setText(note.getCreationDate());
+    }
+
+    private Contract getContract() {
+        return (Contract) getActivity();
+    }
+
+    public interface Contract {
         void saveNote(Note note);
     }
 }
