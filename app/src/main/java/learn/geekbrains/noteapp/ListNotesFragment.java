8ef34b42f010d9d44ea540c4bd3bf41aa6b1662f
anarchyon <protagonyst@mail.ru>
2021-06-22 04:38:25 +0300

    Доработал изменение поведения BottomAppBar

--- app/src/main/java/learn/geekbrains/noteapp/ListNotesFragment.java
+++ app/src/main/java/learn/geekbrains/noteapp/ListNotesFragment.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Parcelable;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
@@ -29,15 +32,9 @@ import java.util.List;
 public class ListNotesFragment extends Fragment {
     private static final String ARG_NOTES = "notes";
     public static final String TAG = "list_fragment";
-
     private List<Note> notes;
     private RecyclerView recyclerView;
     private NoteAdapter adapter;
-    private BottomAppBar bottomAppBar;
-    private FloatingActionButton fab;
-
-    public ListNotesFragment() {
-    }
 
     public static ListNotesFragment newInstance(List<Note> notes) {
         ListNotesFragment listNotesFragment = new ListNotesFragment();
@@ -86,9 +83,8 @@ public class ListNotesFragment extends Fragment {
     }
 
     private void setMenuListener() {
-        bottomAppBar = requireActivity().findViewById(R.id.bottom_menu);
+        BottomAppBar bottomAppBar = requireActivity().findViewById(R.id.bottom_menu);
         if (bottomAppBar != null) {
-            initBottomAppBarAndFabState();
 
             bottomAppBar.setOnMenuItemClickListener(item -> {
                 int itemId = item.getItemId();
@@ -99,30 +95,6 @@ public class ListNotesFragment extends Fragment {
                 return false;
             });
         }
-    }
-
-    private void initBottomAppBarAndFabState() {
-        bottomAppBar.setNavigationIcon(R.drawable.ic_baseline_menu_24);
-        bottomAppBar.getMenu().findItem(R.id.menu_main_search_button).setVisible(true);
-
-        fab = requireActivity().findViewById(R.id.fab);
-        if (fab != null) {
-            fab.setImageResource(R.drawable.ic_baseline_add_24);
-            fab.setOnClickListener(view1 -> getContract().showReceivedNote(null));
-        }
-    }
-
-    @Override
-    public void onPause() {
-        if (bottomAppBar != null) {
-            bottomAppBar.getMenu().findItem(R.id.menu_main_search_button).setVisible(false);
-            bottomAppBar.setNavigationIcon(null);
-        }
-        if (fab != null) {
-            fab.setImageResource(R.drawable.ic_baseline_reply_24);
-            fab.setOnClickListener(view -> requireActivity().onBackPressed());
-        }
-        super.onPause();
     }
 
     private void renderList() {
